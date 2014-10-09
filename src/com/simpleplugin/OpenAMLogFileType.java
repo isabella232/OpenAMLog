d50185f20049b7d40b99d54b43f8e229314210e0
quentin Castel <qcastel@quentins-MacBook-Pro.local>
2014-10-09 12:57:09 +0100

    Plugin version based on tutorial with a beginning of refactor

---
+++ src/com/simpleplugin/OpenAMLogFileType.java
@@ -0,0 +1,39 @@
+package com.simpleplugin;
+
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class OpenAMLogFileType extends LanguageFileType {
+    public static final OpenAMLogFileType INSTANCE = new OpenAMLogFileType();
+
+    private OpenAMLogFileType() {
+        super(SimpleLanguage.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "Simple file";
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Simple language file";
+    }
+
+    @NotNull
+    @Override
+    public String getDefaultExtension() {
+        return "lfr";
+    }
+
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return SimpleIcons.FILE;
+    }
+}
