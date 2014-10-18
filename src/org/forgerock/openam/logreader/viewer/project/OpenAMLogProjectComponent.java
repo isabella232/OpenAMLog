c94e3d0f48d26772d4ec7a78fc6ecedd29de5feb
qcastel <quentincastel86@gmail.com>
2014-10-18 23:12:02 +0100

    Add Copyright for all the project files

--- src/org/forgerock/openam/logreader/viewer/project/OpenAMLogProjectComponent.java
+++ src/org/forgerock/openam/logreader/viewer/project/OpenAMLogProjectComponent.java
@@ -1,24 +1,18 @@
 /*
-    IDEA PsiViewer Plugin
-    Copyright (C) 2002 Andrew J. Armstrong
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-	Author:
-	Andrew J. Armstrong <andrew_armstrong@bigpond.com>
-*/
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright 2014 ForgeRock AS".
+ *
+ * Copyright 2014 ForgeRock AS.
+ */
 package org.forgerock.openam.logreader.viewer.project;
 
 import com.intellij.openapi.actionSystem.ActionManager;
